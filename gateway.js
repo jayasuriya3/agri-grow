7ff83bc33c72af8bdd09a2b8c56d2be4f907e33c
jaiSuriya <suriyashan98@gmail.com>
2023-02-07 21:53:24 +0530

    mongo uri connected

--- gateway.js
+++ gateway.js
@@ -5,10 +5,10 @@ const app = require('express')();
 const { MS_LMS, GATEWAY_PORT } = process.env;
 const PORT = GATEWAY_PORT || 3000;
 
-const lmsHost = MS_LMS === "local" ? 'http://localhost:7000/' : 'https://dev-nucleus.byjusorders.com/';
+const lmsHost = MS_LMS === "local" ? 'http://localhost:7000/' : 'https://dev-agri.grow.com/';
 
 
-app.use('/nucleusapi/loanmanagement/', createProxyMiddleware ({ target: lmsHost, changeOrigin: true }));
+app.use('/agriapi/loanmanagement/', createProxyMiddleware ({ target: lmsHost, changeOrigin: true }));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- server/src/utils/environment.js
+++ server/src/utils/environment.js
@@ -6,5 +6,6 @@ module.exports = {
   appName: 'agri-grow',
   host: process.env.HOST_NAME || 'localhost',
   port: process.env.PORT || 8000,
+  db: process.env.MONGODB_NUCLEUS_URI || 'MONGODB_NUCLEUS_URI',
 
 };
